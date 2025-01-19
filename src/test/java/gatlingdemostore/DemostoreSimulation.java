878a4feffb4dbe7a4576fe86897a581c78726b22
PawelMike <michalkowski.pawel@gmail.com>
2025-01-19 22:35:56 +0100

    problems with percent

--- src/test/java/gatlingdemostore/DemostoreSimulation.java
+++ src/test/java/gatlingdemostore/DemostoreSimulation.java
@@ -2,12 +2,9 @@ package gatlingdemostore;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
-import static io.gatling.javaapi.jdbc.JdbcDsl.*;
-
 
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
-import io.gatling.javaapi.jdbc.*;
 import java.time.Duration;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -15,6 +12,14 @@ public class DemostoreSimulation extends Simulation {
 
   private static final String DOMAIN = "demostore.gatling.io";
   private static final HttpProtocolBuilder HTTP_PROTOCOL = http.baseUrl("https://" + DOMAIN);
+
+  private static final int USER_COUNT = Integer.parseInt(System.getProperty("USERS", "5"));
+
+  private static final Duration RAMP_DURATION =
+    Duration.ofSeconds(Integer.parseInt(System.getProperty("RAMP DURATION", "10")));
+
+  private static final Duration TEST_DURATION = 
+    Duration.ofSeconds(Integer.parseInt(System.getProperty("DURATION", "60")));
 
   private static final FeederBuilder<String> categoryFeeder =
     csv("data/categoryDetails.csv").random();
@@ -137,10 +142,9 @@ public class DemostoreSimulation extends Simulation {
           .on(
             exec(Catalog.Category.view)
               .pause(MIN_PAUSE, MAX_PAUSE)
-              .exec(Catalog.Product.view)
-          );
+              .exec(Catalog.Product.view));
 
-    private static final ChainBuilder abadonedCart = 
+    private static final ChainBuilder abandonedCart = 
       exec(initSession)
         .exec(CmsPages.homepage)
         .pause(MAX_PAUSE)
@@ -167,45 +171,30 @@ public class DemostoreSimulation extends Simulation {
 
   private static class Scenarios {
     private static final ScenarioBuilder defaultPurchase =
-      scenario("Default Load Test")
-        .during(Duration.ofSeconds(60))
-        .on(
-          randomSwitch()
+        scenario("Default Load Test")
+            .during(TEST_DURATION)
             .on(
-              Choice.withWeight(75.0, exec(UserJourneys.browseStore)),
-              Choice.withWeight(15.0, exec(UserJourneys.abadonedCart)),
-              Choice.withWeight(10.0, exec(UserJourneys.completePurchase))));
-    
+                randomSwitch()
+                    .on(percent(75.0), exec(UserJourneys.browseStore))  // 75% dla browseStore
+                    .on(percent(15.0), exec(UserJourneys.abandonedCart)) // 15% dla abandonedCart
+                    .on(percent(10.0), exec(UserJourneys.completePurchase)) // 10% dla completePurchase
+            );
+
     private static final ScenarioBuilder highPurchase =
-      scenario("High Purchase Load Test")
-        .during(Duration.ofSeconds(60))
-          .on(
-            randomSwitch()
-              .on(
-                Choice.withWeight(25.0, exec(UserJourneys.browseStore)),
-                Choice.withWeight(25.0, exec(UserJourneys.abadonedCart)),
-                Choice.withWeight(50.0, exec(UserJourneys.completePurchase))));
-  }
+        scenario("High Purchase Load Test")
+            .during(TEST_DURATION)
+            .on(
+                randomSwitch()
+                    .on(percent(25.0), exec(UserJourneys.browseStore))  // 25% dla browseStore
+                    .on(percent(25.0), exec(UserJourneys.abandonedCart)) // 25% dla abandonedCart
+                    .on(percent(50.0), exec(UserJourneys.completePurchase)) // 50% dla completePurchase
+            );
+}
 
   {
     setUp(
-      scn.injectOpen(constantUsersPerSec(1).during(Duration.ofMinutes(3)))
-        .protocols(HTTP_PROTOCOL)
-        .throttle(
-          reachRps(10).in(Duration.ofSeconds(30)),
-          holdFor(Duration.ofSeconds(60)),
-          jumpToRps(20),
-          holdFor(Duration.ofSeconds(60))))
-      .maxDuration(1600);
+      Scenarios.defaultPurchase
+        .injectOpen(rampUsers(USER_COUNT).during(RAMP_DURATION))
+        .protocols(HTTP_PROTOCOL));
   }        
-  // {
-  // setUp(
-  //   scn.injectOpen(
-  //       atOnceUsers(3),
-  //       nothingFor(5),
-  //       rampUsers(10).during(20),
-  //       nothingFor(10),
-  //       constantUsersPerSec(1).during(20)))
-  //     .protocols(HTTP_PROTOCOL);
-  // }
 }
